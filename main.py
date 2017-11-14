474d71662710516a91d150d185ee8abb2ec1a436
安藤亮介 <crawd4274@gmail.com>
2017-11-14 10:33:19 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# encoding:UTF-8
+
+import serial
+import urllib
+import threading
+
+# POSTするURL
+url = "http://html.takashia.xyz/post.php"
+
+# シリアルポートを指定
+serport = "/dev/ttyAMA0"
+
+
+def post(arg):
+    # 受信したデータを;でわける
+    message = arg.split(";")
+    try:
+        error_checker = message[9]
+        # tweliteから送られてきた情報が送信すべき情報であれば送信
+    except IndexError:
+        print("too short message")
+        return 0
+    except:
+        print("unknown error")
+        return 0
+    try:
+        params = {'uart': arg}
+        data = urllib.urlencode(params)
+        urllib.urlopen(url, data)
+        print("I send a message to " + url + "\nmessage:" + arg)
+        return 0
+    except:
+        print("I try to send a message but missed it")
+        return 0
+    return 0
+
+
+# main program
+# シリアル通信を開始
+
+
+def main():
+    port = serial.Serial(serport, 115200)
+    while True:
+        try:
+            rcv = port.readline()
+            resp = rcv.strip()
+            print(resp)
+            # データを送信する。送信中にシリアルからのアクセスが来て時間が遅れてしまうのを避けるため新しいスレッドを立てる
+            threading.Thread(target=post, args=(resp,)).start()
+        except:
+            print("error")
+
+
+if __name__ == "__main__":
+    main()
